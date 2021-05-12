2668aef168ae446ece2a164b00f611af8becd59e
Wikode Medias <wikodemedias@gmail.com>
2021-05-12 17:11:12 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const getHeadersKeys = () => {
 
 export const client = () => {
   return axios.create({
-    baseURL: BASE_URL,
+    baseURL: 'http://localhost:3001/api/v1/',
     headers: getHeadersKeys()
   })
 }
